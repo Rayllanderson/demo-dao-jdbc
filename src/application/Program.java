c9c7a9dbca93e6bbebb8c2b33a2c430c1aa9e178
Rayllanderson <rayllanderson@gmail.com>
2020-06-18 19:33:18 -0300

    Implementando findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,12 @@ public class Program {
 	Department obj = new Department(1, "Books");
 	
 	Seller seller = new Seller(11, "João", "joao@gmail.com", new Date(), 3000.00, obj);
+	
+	SellerDao sellerDao = DaoFactory.createSellerDao();
+	
+	Seller sell = sellerDao.findById(12);
+	
+	System.out.println(sell);
 	
 	System.out.println(obj);
 	
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,6 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -7,7 +8,7 @@ public class DaoFactory {
     public static SellerDao createSellerDao() {
 	//isso aq é criado pra retornar um jdbc, mas caso eu queria mudar, colocar outro sem ser JDBC, eu só mudo aqui e gg
 	// like new SellerDaoMongoDB. saca? ai ja muda tudo automaticamente lá, xD izi. fácil manutençãp demais
-	return new SellerDaoJDBC();
+	return new SellerDaoJDBC(DB.getConnection());
     }
 
 }
