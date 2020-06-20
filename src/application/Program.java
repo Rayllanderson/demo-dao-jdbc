ef6cd6c8120eed07a35e365e399a41644f443dac
Rayllanderson <rayllanderson@gmail.com>
2020-06-20 02:33:42 -0300

    Projeto Finalizado!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -9,7 +12,8 @@ import model.entities.Seller;
 
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+	SimpleDateFormat sdf = new SimpleDateFormat ("yyyy/MM/dd");
 
 	SellerDao sellerDao = DaoFactory.createSellerDao();
 	
@@ -17,6 +21,34 @@ public class Program {
 	Seller sell = sellerDao.findById(12);
 	
 	System.out.println(sell);
-
+	
+	System.out.println("\n=== Teste número #2 - Seller findByDepartment ===");
+	Department department = new Department (3, null);
+	List<Seller> list = sellerDao.findByDepartment(department);
+	list.forEach(System.out::println);
+	
+	list.clear();
+	System.out.println("\n=== Teste número #3 - Seller findAll ===");
+	list = sellerDao.findAll();
+	list.forEach(System.out::println);
+	
+	/*System.out.println("\n=== Teste número #4 - Seller Insert ===");
+	Seller junin = new Seller(null, "Costa Neto", "fedora@gmail.com", new Date(), 3999.8, department);
+	sellerDao.insert(junin);
+	System.out.println("New id: " + junin.getId());
+	list = sellerDao.findAll();
+	list.forEach(System.out::println);
+	
+	/*System.out.println("\n=== Teste número #5 - Seller update ===");
+	sell = sellerDao.findById(20);
+	sell.setName("Fernando Maranhão");
+	sell.setEmail("beiraMar321@gmail.com");
+	sell.setDepartment(new Department(1, null));
+	sell.setBaseSalary(2499.77);
+	sellerDao.update(sell);
+	list.forEach(System.out::println);*/
+	
+	System.out.println("\n=== Teste número #6 - Seller delet ===");
+	sellerDao.deleteById(55);
     }
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -9,6 +10,10 @@ public class DaoFactory {
 	//isso aq é criado pra retornar um jdbc, mas caso eu queria mudar, colocar outro sem ser JDBC, eu só mudo aqui e gg
 	// like new SellerDaoMongoDB. saca? ai ja muda tudo automaticamente lá, xD izi. fácil manutençãp demais
 	return new SellerDaoJDBC(DB.getConnection());
+    }
+    
+    public static DepartmentDao createDepartmentDao() {
+	return new DepartmentDaoJDBC(DB.getConnection());
     }
 
 }
